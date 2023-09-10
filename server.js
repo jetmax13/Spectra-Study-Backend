0b9b50829fc74ff909c0925c8e951af9d41abde2
jetmax13 <vaibhavduggal03@gmail.com>
2023-09-11 00:55:54 +0530

    resolving deployment issues

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const errorHandler = require('./middleware/errorhandler');
+const errorHandler = require('./middleware/errorHandler');
 const dotenv=require('dotenv').config();
 const connectDb= require('./config/dbConnection');
 
