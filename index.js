47537a5664651b392673d6c5f3f83b76b2750265
jetmax13 <vaibhavduggal03@gmail.com>
2023-09-11 12:49:03 +0530

    corsoptions added

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express=require('express');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv=require('dotenv').config();
 const connectDb= require('./config/dbConnection');
+const cors=require("cors");
+const corsOptions={
+    origin:"*",
+};
 
 const app=express();
+app.use(cors(corsOptions));
 const port=process.env.PORT;
 
 connectDb();
